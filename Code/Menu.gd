daeff4b89f23778cdfe766418210cf07163c3a63
Masonzk <masonzkaplan@gmail.com>
2023-03-17 11:04:28 -0400

    Added functionality to null zones.

--- Code/Menu.gd
+++ Code/Menu.gd
@@ -1,12 +1,10 @@
 extends Control
 
-
 func _on_Button_pressed():
-	get_tree().change_scene("res://Scenes/Levels/Base.tscn")
+	get_tree().change_scene("res://Scenes/Levels/1.tscn")
 
 func _on_Button2_pressed():
 	get_tree().change_scene("res://Scenes/Levels/2.tscn")
-
 
 func _on_Button3_pressed():
 	get_tree().change_scene("res://Scenes/Levels/3.tscn")
@@ -16,3 +14,18 @@ func _on_Button4_pressed():
 
 func _on_Button5_pressed():
 	get_tree().change_scene("res://Scenes/Levels/5.tscn")
+
+func _on_Button6_pressed():
+	get_tree().change_scene("res://Scenes/Levels/6.tscn")
+
+func _on_Button7_pressed():
+	get_tree().change_scene("res://Scenes/Levels/7.tscn")
+
+func _on_Button8_pressed():
+	get_tree().change_scene("res://Scenes/Levels/8.tscn")
+
+func _on_Button9_pressed():
+	get_tree().change_scene("res://Scenes/Levels/9.tscn")
+
+func _on_Button10_pressed():
+	get_tree().change_scene("res://Scenes/Levels/10.tscn")
---
+++ Code/NullZone.gd
@@ -0,0 +1,6 @@
+extends StaticBody2D
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("ball"):
+		body.get_node("CollisionShape2D").disabled = true
+		body.get_node("AnimationPlayer").play("null")
