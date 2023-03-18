377cc140d14260f5b4a2a901fa00b018901f1396
Masonzk <masonzkaplan@gmail.com>
2023-03-17 22:22:54 -0400

    Added gravity towards collection zone when completed. Fixed bug with ball not registering if value change while inside collector.

--- Code/Collector.gd
+++ Code/Collector.gd
@@ -5,8 +5,11 @@ export var desired_value : float
 func _ready():
 	$Label.text = str(desired_value)
 	
-func _on_Area2D_body_entered(body):
-	if body.is_in_group("ball"):
-		if body.current_value == desired_value:
-			body.destroy()
-			get_parent().level_completed()
+func _process(delta):
+	for body in $Area2D.get_overlapping_bodies():
+		if body.is_in_group("ball"):
+			if body.current_value == desired_value:
+				body.destroy()
+				body.get_node("CollisionShape2D").disabled = true
+				body.linear_velocity = (global_position - body.global_position) * 5
+				get_parent().level_completed()
